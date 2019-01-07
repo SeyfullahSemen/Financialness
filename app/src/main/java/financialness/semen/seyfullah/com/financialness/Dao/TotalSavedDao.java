0e57916041f8b59b8afc333676b535a898030799
seyf <seyfo12@hotmail.com>
2019-01-07 13:53:33 +0100

    Added comments to different files

--- app/src/main/java/financialness/semen/seyfullah/com/financialness/Dao/TotalSavedDao.java
+++ app/src/main/java/financialness/semen/seyfullah/com/financialness/Dao/TotalSavedDao.java
@@ -14,13 +14,13 @@ import financialness.semen.seyfullah.com.financialness.Entity.TotalSaved;
  */
 @Dao
 public interface TotalSavedDao {
-
+    // Select everything from the totalsaved table
     @Query("SELECT * FROM TotalSaved")
     public LiveData<List<TotalSaved>> getTotalSaved();
-
+    // Insert a new totalsaved into the table
     @Insert
     void insertNewTotalSaved(TotalSaved... totalsaved);
-
+    // Query to update the already set totalsaved value.
     @Query("UPDATE TotalSaved SET totalsaved= :totalsaved WHERE TotalSavedId = :id")
     void updateCurrentSaved(int id, double totalsaved);
 }
